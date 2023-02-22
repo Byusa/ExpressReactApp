e6d13313761e722b9e0f231d3f51a5964fea3276
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2023-02-22 12:58:23 -0800

    made some changes

--- server/server.js
+++ server/server.js
@@ -4,4 +4,6 @@ const app = express()
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
-app.listen(5000, () => { console.log("Server started on port 5000")})
+app.listen(3000, () => {
+    console.log("Server started on port 3000")
+})
